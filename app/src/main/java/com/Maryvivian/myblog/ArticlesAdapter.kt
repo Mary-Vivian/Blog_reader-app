e569c960ec031ed759145b373754494cf39516df
Mary-Vivian <muthonimaryvivian>
2024-06-30 21:32:47 +0300

    first commit

--- app/src/main/java/com/Maryvivian/myblog/ArticlesAdapter.kt
+++ app/src/main/java/com/Maryvivian/myblog/ArticlesAdapter.kt
@@ -1,27 +1,35 @@
 package com.Maryvivian.myblog
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ArticlesAdapter(val aritcles: List<Articles>):RecyclerView.Adapter<ArticlesViewHolder>() {
+class ArticlesAdapter(val articles: List<Articles>):RecyclerView.Adapter<ArticlesViewHolder>() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticlesViewHolder {
       var itemView=LayoutInflater.from(parent.context).inflate(R.layout.artical_item, parent, false)
         return ArticlesViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        return aritcles.size
+        return articles.size
 
     }
 
     override fun onBindViewHolder(holder: ArticlesViewHolder, position: Int) {
-        val article=aritcles[position]
+        val article=articles[position]
+        holder.tvArticle.text=article.id
+        holder.tvArticle.text=article.title
         holder.tvName.text=article.author
         holder.tvPreview.text=article.articlePreview
-        holder.tvArticle.text=article.title
+        holder.tvArticle.text=article.authorProfilePicture
+        holder.tvArticle.text=article.publishingDate
+        holder.tvArticle.text=article.articlePhoto
+        holder.tvArticle.text=article.link
+
 
     }
 }
--- app/src/main/java/com/Maryvivian/myblog/MainActivity.kt
+++ app/src/main/java/com/Maryvivian/myblog/MainActivity.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.Maryvivian.myblog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvarticles.layoutManager=LinearLayoutManager(this)
+        displayArticles()
+
+//        val  articlesList= listOf(
+//            Articles("1", "Tales tolly", "Silver Jinat", "https://www.pinterest.com/pin/684899055831224871/","2004-06-30","This article gives insight abou a mythical creature with ten heads","https://www.pinterest.com/pin/684899055831224871/","https://www.theschooloflife.com/article/the-importance-of-books/",)
+//
+//        )
+//
+//        val recyclerView=binding.rvarticles
+//        val adapter=ArticlesAdapter(articlesList)
+//        recyclerView.adapter=adapter
+//        recyclerView.layoutManager=LinearLayoutManager(this)
+
+    }
+
+    fun displayArticles(){
+        val text=Articles("1","Hurricane","Tyla","Tall","2024","2024","343","https://www.pinterest.com/pin/684899055831224871/")
+        val text1=Articles("1","Hurricane","Tyla","Tall","2024","2024","343","https://www.pinterest.com/pin/684899055831224871/")
+        val text2=Articles("1","Hurricane","Tyla","Tall","2024","2024","343","https://www.pinterest.com/pin/684899055831224871/")
+        val text3=Articles("1","Hurricane","Tyla","Tall","2024","2024","343","https://www.pinterest.com/pin/684899055831224871/")
+        val text4=Articles("1","Hurricane","Tyla","Tall","2024","2024","343","https://www.pinterest.com/pin/684899055831224871/")
+        val text5=Articles("1","Hurricane","Tyla","Tall","2024","2024","343","https://www.pinterest.com/pin/684899055831224871/")
+
+        val articles= listOf(text1,text2,text3,text4,text5)
+        val articlesAdapter=ArticlesAdapter(articles)
+        binding.rvarticles.adapter=articlesAdapter
     }
 }
